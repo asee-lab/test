672926df520910affe4695808f40ad8a96fa0d8f
Jove Stefanovski <Jove.Stefanovski@asee.io>
2025-07-08 10:45:57 +0200

    fix build

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-app.MapGet("/", () => "Hello World!");
+namespace VulnerableApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+            builder.Services.AddControllers();
+            var app = builder.Build();
+
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
